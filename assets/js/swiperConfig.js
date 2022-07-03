7dd33d81a9e4893f66365f3bd64e6e57fe6519e0
Aldilan <aldilan108@gmail.com>
2022-07-03 21:13:01 +0700

    80%

--- assets/js/swiperConfig.js
+++ assets/js/swiperConfig.js
@@ -1,24 +1,64 @@
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'vertical',
+let swiperBottomScrollbarFull = new Swiper('.swiper', {
+  
   loop: true,
-
-  // If we need pagination
+  allowTouchMove: true,
+  slidesPerView: 'auto',
+  grabCursor: true,
+  preventClicks: true,
+  spaceBetween: 30,
+  keyboardControl: true,
+  speed: 1000,
   pagination: {
-    el: '.swiper-pagination',
+      el: null
   },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
   scrollbar: {
-    el: '.swiper-scrollbar',
+      el: '.swiper-scrollbar',
+      draggable: true,
+      hide: false,
+      snapOnRelease: true
   },
-  autoplay: {
-    delay: 3000,
+  mousewheel: {
+      enable: true
   },
+  keyboard: {
+      enabled: true
+  },
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+  },
+  breakpoints: {
+      767: {
+          scrollbar: {
+              hide: true
+          },
+          spaceBetween: 0,
+          autoHeight: true,
+          centeredSlides: false,
+          slidesOffsetAfter: 85
+      }
+  },
+});
+
+var swiper2 = new Swiper('.swiper2', {
+  loop:true,
+  direction:'vertical',
+  allowTouchMove: true,
+  slidesPerView: 'auto',
+  grabCursor: true,
+  preventClicks: true,
+  spaceBetween: 30,
+  keyboardControl: true,
+  speed: 500,
+  autoHeight: true,
+  pagination:  {
+    el: '.swiper-pagination2',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next2',
+    prevEl: '.swiper-button-prev2',
+  },
+  // Enable debugger
+  debugger: true,
 });
